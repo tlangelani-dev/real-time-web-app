f1049be11016fba243a958f57ae78eb13a88b632
Tlangelani Mboweni <mbowenit@gmail.com>
2017-07-21 15:49:28 +0200

    use express static middleware

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
+const morgan = require('morgan');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 require('dotenv').config();
 const PORT = process.env.HTTP_PORT;
 
 // middlewares
 app.use(morgan('combined'));
-
-app.get('/', (req, res) => {
-    console.log(req, res);
-    res.send('Listing Categories!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`Express server running on port ${PORT}`);
